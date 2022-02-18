62d5aa5f1a1dfc7b9c34731a848ac77ab1b1db8f
Shahzan Sadick <sam.shahzan@gmail.com>
2022-02-18 21:21:00 +0530

    consumer updated

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,29 +1,29 @@
 import json
-
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' %self.room_name
+        self.room_group_name = 'chat_%s' % self.room_name
 
-        #join room group
+        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
         self.accept()
-    
-    def disconnect(self,close_code):
+
+    def disconnect(self, close_code):
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    def recieve(self,text_data):
+    # Receive message from WebSocket
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
@@ -35,7 +35,7 @@ class ChatConsumer(WebsocketConsumer):
                 'message': message
             }
         )
-    
+
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
